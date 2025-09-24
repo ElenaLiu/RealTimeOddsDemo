351907afc5c138ddcdea9d6d39017f24994b5f5d
elena.liu <elena.liu@baifu-tech.net>
2025-09-24 20:43:08 +0800

    Initial Commit

---
+++ RealTimeOdds/RealTimeOddsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealTimeOddsApp.swift
+//  RealTimeOdds
+//
+//  Created by Elena on 2025/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RealTimeOddsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
